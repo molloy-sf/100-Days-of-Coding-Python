6c149affe03d40c7491bf391097dd665a8532ac6
molloy-sf <145492833+molloy-sf@users.noreply.github.com>
2024-08-27 14:41:31 -0400

    love calculator
    Working with loops and print statement with variables

---
+++ loveCalculator/main.py
@@ -0,0 +1,57 @@
+## My code:
+print("The Love Calculator is calculating your score...")
+name1 = input()  # What is your name?
+name2 = input()  # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+name1_count = 0
+name2_count = 0
+both_names = [name1.lower(), name2.lower()]
+
+for each_name in both_names:
+  for letter in each_name:
+    if letter == "t" or letter == "r" or letter == "u" or letter == "e":
+      name1_count += 1
+
+for each_name in both_names:
+  for letter in each_name:
+    if letter == "l" or letter == "o" or letter == "v" or letter == "e":
+      name2_count += 1
+string_score = str(name1_count) + str(name2_count)
+love_score = int(string_score)
+
+if love_score < 10 or love_score > 90:
+  print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif love_score >= 40 and love_score <= 50:
+  print(f"Your score is {love_score}, you are alright together.")
+else:
+  print(f"Your score is {love_score}.")
+
+
+
+## Teacher's code:
+print("The Love Calculator is calculating your score...")
+name1 = input()  # What is your name?
+name2 = input()  # What is their name?
+# Your code below this line 👇
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
